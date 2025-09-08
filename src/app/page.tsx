6cb4525f41cb6b38d41ec17724b0b6165fe10519
ProXpressions <proxpressions8@gmail.com>
2025-09-08 18:00:52 +0530

    adds header, routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="bg-black">
-      <main>
-        <div>
-          <h1>
+    <div className="bg-black bg-[url('/images/home-img.png')] bg-cover bg-center">
+      <main className="flex flex-col justify-center text-center max-w-5xl mx-auto h-dvh">
+        <div className="flex flex-col gap-6 p-12 rounded-xl bg-black/90 text-white w-4/5 sm:max-w-96 mx-auto sm:text-2xl">
+          <h1 className="text-4xl font-bold">
             Dan&apos;s Computer <br /> Repair Shop
           </h1>
           <address>
